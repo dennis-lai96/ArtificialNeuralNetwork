8d610a8f6141e1e0f11baad34a3fafa508d4c5ac
dennis-lai96 <dennis.lai96@gmail.com>
2023-03-12 19:03:38 -0700

    reading file

--- ArtificialNeuralNetwork.py
+++ ArtificialNeuralNetwork.py
@@ -1,1 +1,11 @@
-print("I HAVE NO IDEA WHAT I'M DOING ")
+class Node:
+    def __init__(self,connections):
+        self.collector = 0.0
+        self.connections = connections
+
+if __name__ == "__main__":
+    network_structure = []
+    f = open("NumNodes", "r")
+
+    print(f.read().split(','))
+    
