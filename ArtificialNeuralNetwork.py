a64373dd19478ce8e8ea11d3d8ff23e4244ebeb9
dennis-lai96 <124309643+dennis-lai96@users.noreply.github.com>
2023-03-18 14:31:19 -0700

    Update ArtificialNeuralNetwork.py

--- ArtificialNeuralNetwork.py
+++ ArtificialNeuralNetwork.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self,connections):
         self.collector = 0.0
-        self.connections = connections
+        self.connections = None
 def print_array_of_arrays(arrays):
     for array in arrays:
         print(array)
@@ -10,17 +10,24 @@ def print_array_of_arrays(arrays):
 if __name__ == "__main__":
     network_structure = []
     f = open("NumNodes", "r")
-    #I literally for the life of me cannot figure out this stupid ass list to array shit. SKIP FOR NOW
+    #figure out txt to array later...
     print(f.read().split(','))
+
 
     network_structure = [4,2,1]
     initial_values =  [4.1, 5.5, 3.3, 10.01 ]
     #Now we need to make an empty Node array of arrays using this.
 
     network = [ [0] * size for size in network_structure]
+
+    node = ([Node])
+
+    network[0][0] = node
+
+
+
     print_array_of_arrays(network)
-
-
+    print(type(network))
 
     #The loop should look seomthing like
     #for: int i = 0, i < network_list_max(the size of the array of arrays), i++
@@ -28,3 +35,15 @@ if __name__ == "__main__":
         #if int i > 0, array[i] receives array[i-1]'s first address.
         #afterwards, array[i] nodes all receive in accumulator, all the values from previous arrays accumulators.
 
+    # for i in range(len(network)):
+    #     if i==0:
+    #         for j in range(len(initial_values)):
+    #             print()
+
+                #network[0][j] = initial_values[j]
+
+
+
+
+
+    print_array_of_arrays(network)
