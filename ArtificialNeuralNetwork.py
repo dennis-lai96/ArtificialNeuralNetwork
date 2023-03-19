4fee11672f76013deb2016d0126e277facf1c376
dennis-lai96 <dennis.lai96@gmail.com>
2023-03-18 19:41:07 -0700

    Opens node structure and inital values from files

--- ArtificialNeuralNetwork.py
+++ ArtificialNeuralNetwork.py
@@ -9,6 +9,8 @@ def print_array_of_arrays(arrays):
         print(array)
 
 
+
+
 if __name__ == "__main__":
     network_structure = []
 
@@ -16,21 +18,17 @@ if __name__ == "__main__":
     f = open("NumNodes", "r")
     network_structure = [int(num) for num in f.read().split(",")]
     print(network_structure)
-
+    f.close()
     #Open up the file and turn it in to a float list.
-
+    f=open("InitialValues","r")
+    initial_values= [float(num) for num in f.read().split(",")]
     
-    print("Network structure is of type" , type(network_structure))
-    initial_values = [4.1, 5.5, 3.3, 10.01]
-    # Now we need to make an empty Node array of arrays using this.
 
+    # Now we need to make an empty array of arrays using this.
     network = [[0] * size for size in network_structure]
     numLayers = len(network)
     print("nubmer of layers: ", numLayers)
     print_array_of_arrays(network)
-    print(type(network))
-    print("array[1] is ", network[1])
-
 
 
     for i in range(numLayers):
@@ -45,6 +43,7 @@ if __name__ == "__main__":
 
                 print("Network", i, j, "has this connection:", network[0][j].connections)
                 print("Network", i, j, " has this collector val: ", network[0][j].collector)
+
         #hidden layer
         elif i < numLayers-1:  # i is going to be 1 in this case, which is smaller than 2
             print("Network ", i, ": ")
@@ -54,7 +53,6 @@ if __name__ == "__main__":
                         network[i][j].collector = network[i][j].collector+ network[i-1][k].collector
                     print("Network", i, j, "has this connection:", network[i][j].connections)
                     print("Network", i, j, " has this collector val: ", network[i][j].collector)
-
 
         #Last Layer
         else:
@@ -68,11 +66,13 @@ if __name__ == "__main__":
                     network[i][j].collector = network[i][j].collector+ network[i-1][k].collector
                     print("Network", i, j, " has this collector val: ", network[i][j].collector)
 
+    for i in range(numLayers):
+        for j in range(len(network[i])):
+            print(network[i][j].collector)
+
 
 
     print("this is network 0 ", network[0])
-
-
     print("This is network 0-0 connections: ", network[0][0].connections)
     print("This is network 0-1 connections: ", network[0][1].connections)
     print("This is network 0-2 connections: ", network[0][2].connections)
@@ -83,6 +83,7 @@ if __name__ == "__main__":
     print("This is network 1-1 connections: ", network[1][1].connections)
 
     print("this is network 2 ", network[2])
+
     print("value [0][0]", network[0][0].collector)
     print("value [0][1]", network[0][1].collector)
     print("value [0][2]", network[0][2].collector)
