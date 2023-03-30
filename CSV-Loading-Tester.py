f6bb76ac383ac0af24994a52eee037d392447584
dennis-lai96 <dennis.lai96@gmail.com>
2023-03-30 13:08:50 -0700

    in classs edits

--- CSV-Loading-Tester.py
+++ CSV-Loading-Tester.py
@@ -1,7 +1,16 @@
 import csv
+import sqlite3
 
 #okay this does not work lmao
-with open("Data/A_Z Handwritten Data.csv", 'r') as file:
-    csvreader = csv.reader(file)
-    for row in csvreader:
-        print(row)
+
+
+# with open("Data/A_Z Handwritten Data.csv", 'r') as file:
+#     csvreader = csv.reader(file)
+#     for row in csvreader:
+#         print(row)
+
+
+columns = []
+for i in range(764):
+    columns.append(f"a{i}")
+print(f"letter,{','.join(columns)}")
